53f43f88998bccf9ea97022caac1cda470b99560
ruthvikyadav <89997315+ruthvikyadav@users.noreply.github.com>
2021-09-03 18:56:04 -0500

    Update and rename SecondMessageSubscriber.java to SecondSubscriber.java

--- SecondSubscriber.java
+++ SecondSubscriber.java
@@ -3,10 +3,10 @@ package com.pattern.observer.impl;
 import com.pattern.message.Message;
 import com.pattern.observer.Observer;
 
-public class SecondMessageSubscriber implements Observer 
+public class SecondSubscriber implements Observer 
 {
     @Override
     public void updateObserver(Message message) {
-        System.out.println("Messsage for Second subsscriber :: " + message.getMessage());
+        System.out.println("message for Second subscriber :: " + message.getMessage());
     }
 }
