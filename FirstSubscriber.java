3fc64349437480c83415187c3c203e44197c1fc2
ruthvikyadav <89997315+ruthvikyadav@users.noreply.github.com>
2021-09-03 18:54:46 -0500

    Update and rename FirstMessageSubscriber.java to FirstSubscriber.java

--- FirstSubscriber.java
+++ FirstSubscriber.java
@@ -3,7 +3,7 @@ package com.pattern.observer.impl;
 import com.pattern.message.Message;
 import com.pattern.observer.Observer;
 
-public class FirstMessageSubscriber implements Observer 
+public class FirstSubscriber implements Observer 
 {
     public void updateObserver(Message message) {
         System.out.println("Messsage for first subsscriber :: " + message.getMessage());
